f7a2e501acab3681e6792e9eaac4665ff4ff8057
Mardiya Mardha <mardhamardiya.18@gmail.com>
2020-08-13 17:06:36 +0800

    setting adminLTE

---
+++ resources/views/adminlte/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.0.5
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+</footer>
+<!-- jQuery -->
+<script src="{{asset('/adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('/adminlte/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('/adminlte/dist/js/demo.js')}}"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +13,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('adminlte.master');
 });
